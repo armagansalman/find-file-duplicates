9a8fedf1e0447cee4bb0f60423ceeb114ff4b9f5
Armağan Salman <4987651+armaganslmn@users.noreply.github.com>
2021-07-08 13:58:48 +0300

    Can scan multiple paths.

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     input_csv = "input_paths.csv"
     rows = csvh.read_all_csv(input_csv)
     print(rows)
-    found_paths = util.get_fpaths_from_all_paths(rows[0])
+    found_paths = util.get_fpaths_from_all_paths(map(lambda x: x[-1], rows)) # last element of a row is the path
     
     """
     for p in found_paths:
